74a1f3109a61287c0394701cd01d39853deed114
RahulARanger <saihanumarahul66@gmail.com>
2026-08-03 21:38:29 +0530

    fix: minor fixes on setup

--- first_setup.sh
+++ first_setup.sh
@@ -1,4 +1,22 @@
 bash scripts/download_brats2019.sh --output-dir dataset/brats2019
-bash scripts/slicer.sh --dataset-root /Users/rahul/Documents/HFF/dataset/brats2019/extracted --output-dir /Users/rahul/Documents/HFF/dataset/brats2019/splits/explore  --limit 20
+bash scripts/slicer.sh --dataset-root dataset/brats2019/extracted/MICCAI_BraTS_2019_Data_Training --output-dir dataset/brats2019/splits/explore  --limit 20
 python scripts/generate_low_freq.py --path dataset/brats2019/splits/explore
 bash scripts/generate_high_freq.sh --path dataset/brats2019/splits/explore
+
+WANDB_MODE=offline python train.py \
+  --train_list "dataset/brats2019/splits/explore/train.txt" \
+  --val_list "dataset/brats2019/splits/explore/validation.txt" \
+  --dataset_name brats19 \
+  --class_type et \
+  --num_epochs 1 \
+  --batch_size 1
+
+
+
+  WANDB_MODE=offline python train.py \
+  --train_list "dataset/brats2019/splits/explore/train.txt" \
+  --val_list "dataset/brats2019/splits/explore/validation.txt" \
+  --dataset_name brats19 \
+  --class_type et \
+  --num_epochs 350 \
+  --batch_size 1
